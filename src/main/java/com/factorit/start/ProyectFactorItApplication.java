f58c0a9cdd69e3d139df37ce757b114ed1fe61ec
valenchu <valecassino1@gmail.com>
2022-09-14 20:33:08 -0300

    data

---
+++ src/main/java/com/factorit/start/ProyectFactorItApplication.java
@@ -0,0 +1,13 @@
+package com.factorit.start;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProyectFactorItApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProyectFactorItApplication.class, args);
+	}
+
+}
